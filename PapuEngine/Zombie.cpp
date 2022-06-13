e37b12c6e66f785467ceccb965caa3d2f8e25d19
ZuxAlone <rmagdev20@gmail.com>
2022-06-13 00:00:02 -0500

    Initial commit

---
+++ PapuEngine/Zombie.cpp
@@ -0,0 +1,20 @@
+#include "Zombie.h"
+
+Zombie::Zombie()
+{
+}
+
+
+Zombie::~Zombie()
+{
+}
+
+void Zombie::init(float speed, glm::vec2 position) {
+	_speed = speed;
+	_color.set(0, 185, 0, 255);
+	_position = position;
+}
+
+void Zombie::update(const std::vector<std::string>& levelData) {
+
+}
---
+++ PapuEngine/Zombie.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Agent.h"
+class Zombie : public Agent
+{
+public:
+	Zombie();
+	~Zombie();
+
+	void init(float speed, glm::vec2 position);
+	void update(const std::vector<std::string>& levelData);
+};
+
